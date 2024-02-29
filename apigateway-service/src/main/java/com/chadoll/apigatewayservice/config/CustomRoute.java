7ddab10b90bbba355dfb4d76a106099f6e9ebe54
김부경 <sa01023@naver.com>
2024-03-01 01:11:45 +0900

    [FEAT] 게이트웨이 라우팅 설정

--- apigateway-service/src/main/java/com/chadoll/apigatewayservice/config/CustomRoute.java
+++ apigateway-service/src/main/java/com/chadoll/apigatewayservice/config/CustomRoute.java
@@ -6,17 +6,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 // Config 클래스를 활용한 경로 설정
-@Configuration
-public class CustomRoute {
-
-    @Bean
-    public RouteLocator mstRoute(RouteLocatorBuilder builder) {
-
-        return builder.routes()
-                .route("user", r -> r.path("/user/**")
-                        .uri("http://localhost:8081"))
-                .route("community", r -> r.path("/community/**")
-                        .uri("http://localhost:8082"))
-                .build();
-    }
-}
+//@Configuration
+//public class CustomRoute {
+//
+//    @Bean
+//    public RouteLocator mstRoute(RouteLocatorBuilder builder) {
+//
+//        return builder.routes()
+//                .route("user", r -> r.path("/user/**")
+//                        .uri("http://localhost:8081"))
+//                .route("community", r -> r.path("/community/**")
+//                        .uri("http://localhost:8082"))
+//                .build();
+//    }
+//}
--- community-service/src/main/java/com/chadoll/communityservice/controller/CommunityController.java
+++ community-service/src/main/java/com/chadoll/communityservice/controller/CommunityController.java
@@ -1,6 +1,7 @@
 package com.chadoll.communityservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
