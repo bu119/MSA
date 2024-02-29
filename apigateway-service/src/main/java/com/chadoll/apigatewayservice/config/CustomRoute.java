6e0bff98abbd9552b92f5bd03b3e2dc733924116
김부경 <sa01023@naver.com>
2024-02-29 23:58:08 +0900

    [FEAT] Config 클래스를 활용한 경로 설정

--- apigateway-service/src/main/java/com/chadoll/apigatewayservice/config/CustomRoute.java
+++ apigateway-service/src/main/java/com/chadoll/apigatewayservice/config/CustomRoute.java
@@ -1,2 +1,22 @@
-package com.chadoll.apigatewayservice.config;public class CustomRoute {
+package com.chadoll.apigatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// Config 클래스를 활용한 경로 설정
+@Configuration
+public class CustomRoute {
+
+    @Bean
+    public RouteLocator mstRoute(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route("user", r -> r.path("/user/**")
+                        .uri("http://localhost:8081"))
+                .route("community", r -> r.path("/community/**")
+                        .uri("http://localhost:8082"))
+                .build();
+    }
 }
